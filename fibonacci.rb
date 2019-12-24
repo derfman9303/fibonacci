7b3a803905ad59081cc67bac4df431e76d0b6cdd
derfman9303 <frederichodges@gmail.com>
2019-12-24 01:47:11 +0000

    initial commit

---
+++ fibonacci.rb
@@ -0,0 +1,41 @@
+require 'benchmark'
+
+class Fibonacci
+  def initialize(length)
+    @length = length
+    @array = []
+  end
+puts Benchmark.measure {
+  puts "recursive method:"
+  def recursive_fib(iteration=0, sum=0, previous=1)
+    if iteration < @length
+      sum = sum + previous
+      previous = sum - previous
+      iteration = iteration + 1
+      recursive_fib(iteration, sum, previous)
+    else
+      return sum
+    end
+  end
+}
+puts Benchmark.measure {
+  puts "iterative method:"
+  def iterative_fib
+    if @array.empty?
+      @array << 1
+      @array << 0
+    end
+    @length.times do
+      sum = @array[-1] + @array[-2]
+      @array << sum
+    end
+    return @array[-1]
+  end
+}
+end
+
+
+
+fib = Fibonacci.new(15)
+#puts fib.recursive_fib
+#puts fib.iterative_fib
